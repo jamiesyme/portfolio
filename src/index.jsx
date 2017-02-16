377aa7149b94223f6d08364698a0b7e5da7c334a
Jamie Syme <jamie.d.syme@gmail.com>
2017-02-15 19:33:09 -0800

    Improved page layout (WIP).

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const colorScheme = {
+	body:     '#494949',
+	accent:   '#FF3B3F',
+	heavy:    '#A9A9A9',
+	bg:       '#EFEFEF',
+	bgAccent: '#CAEBF2'
+};
+
+const fontScheme = {
+	heavy: ['Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', 'Tahoma', 'sans-serif'],
+	//heading: ['Tahoma', 'Geneva', 'sans-serif'],
+	body: ['Georgia', 'Serif']
+};
+
+const headingFontStyle = {
+	fontFamily: fontScheme.heavy,
+	fontWeight: 500,
+	color: colorScheme.heavy
+};
+
+const bodyFontStyle = {
+	fontFamily: fontScheme.body,
+	fontSize: 18,
+	lineHeight: 1.6,
+	letterSpacing: 0.5,
+	color: colorScheme.body
+};
+
+
 class Header extends React.Component {
 	render() {
-		const headerStyle = {
+		const padding = 20;
+		const outerStyle = {
+			width: '100%',
+			position: 'fixed',
+			backgroundColor: colorScheme.bg
+		};
+		const innerStyle = {
 			width: 960,
 			margin: 'auto',
-			paddingTop: 20,
-			paddingBottom: 20
+			height: 64
 		};
 		return (
-			<header style={headerStyle}>
-				{this.renderLogo()}
-				{this.renderNav()}
+			<header style={outerStyle}>
+				<div style={innerStyle}>
+					{this.renderLogo()}
+					{this.renderNav()}
+				</div>
 			</header>
 		);
 	}
 	renderLogo() {
-		const logoStyle = {
-			fontFamily: ['Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', 'Tahoma', 'sans-serif'],
+		const logoStyle = Object.assign({}, headingFontStyle, {
 			fontSize: 24,
-			fontWeight: 500,
 			textDecoration: 'none',
-			color: 'black',
-			display: 'inline-block'
-		};
+			display: 'inline-block',
+			height: '100%',
+			
+		});
 		return (
 			<a href="#" style={logoStyle}>
 				jamiesyme.com
@@ -35,20 +71,20 @@ class Header extends React.Component {
 		const navStyle = {
 			float: 'right'
 		};
-		const aStyle = {
-			fontFamily: ['Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', 'Tahoma', 'sans-serif'],
+		const aStyle = Object.assign({}, headingFontStyle, {
 			fontSize: 20,
-			fontWeight: 500,
 			textDecoration: 'none',
-			color: 'black',
 			padding: 2,
 			marginLeft: 20,
 			marginRight: 20,
 			display: 'inline-block'
-		};
+		});
+		const aStyleAccent = Object.assign({}, aStyle, {
+			color: colorScheme.accent
+		});
 		return (
 			<nav style={navStyle}>
-				<a href="#" style={aStyle}>Home</a>
+				<a href="#" style={aStyleAccent}>Home</a>
 				<a href="#" style={aStyle}>Projects</a>
 				<a href="#" style={aStyle}>About</a>
 				<a href="#" style={aStyle}>Contact</a>
@@ -57,24 +93,100 @@ class Header extends React.Component {
 	}
 }
 
-class HomePage extends React.Component {
+
+class Page extends React.Component {
 	render() {
+		const outerStyle = {
+			width: '100vw',
+			height: '100vh',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center'
+		};
+		const hStyle = Object.assign({}, headingFontStyle, {
+			fontSize: 64,
+			//paddingTop: 60,
+			paddingBottom: 60,
+			margin: 0,
+			textAlign: 'center'
+		});
 		return (
-			<div>
-				<Header/>
-				{this.renderGreeting()}
-			</div>
-		);
-	}
-	renderGreeting() {
-		return (
-			<div>
+			<div style={outerStyle}>
+				<div>
+					<h1 style={hStyle}>{this.props.heading}</h1>
+					<div>{this.props.children}</div>
+				</div>
 			</div>
 		);
 	}
 }
 
+
+class HomePage extends React.Component {
+	render() {
+		const pStyle = Object.assign({}, bodyFontStyle, {
+			width: 450,
+			margin: 'auto',
+			textAlign: 'center'
+		});
+		return (
+			<Page heading="Welcome">
+				<p style={pStyle}>I'm a backend web developer who is blah blah here are some more words to make this the right length. Let's add a little bit more and see if we can get a couple more lines.</p>
+			</Page>
+		);
+	}
+}
+
+
+class ProjectsPage extends React.Component {
+	render() {
+		return (
+			<Page heading="Projects">
+				<p style={bodyFontStyle}>I need some projects here</p>
+			</Page>
+		);
+	}
+}
+
+
+class AboutPage extends React.Component {
+	render() {
+		return (
+			<Page heading="About">
+				<p style={bodyFontStyle}>I need to write some stuff about me here</p>
+			</Page>
+		);
+	}
+}
+
+
+class ContactPage extends React.Component {
+	render() {
+		return (
+			<Page heading="Contact">
+				<p style={bodyFontStyle}>I need to write contact info here</p>
+			</Page>
+		);
+	}
+}
+
+
+class Website extends React.Component {
+	render() {
+		return (
+			<div>
+				<Header/>
+				<HomePage/>
+				<ProjectsPage/>
+				<AboutPage/>
+				<ContactPage/>
+			</div>
+		);
+	}
+}
+
+
 ReactDOM.render(
-	<HomePage/>,
+	<Website/>,
 	document.getElementById('root')
 );
