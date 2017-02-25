3dbf027f4427077bbe228596c49e49310587003a
Jamie Syme <jamie.d.syme@gmail.com>
2017-02-25 08:09:11 -0800

    Moved about details into props.

--- client/ecosystems/about.js
+++ client/ecosystems/about.js
@@ -17,16 +17,29 @@ const baseStyles = {
 
 export class AboutEco extends React.Component {
 	render() {
-		const lorem1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut erat vitae turpis fringilla ornare non id leo. Phasellus lacinia turpis non velit lobortis dictum. Integer tempor, mauris sit amet consequat maximus, felis justo sollicitudin turpis, a cursus nulla massa in turpis. Maecenas nibh quam, finibus ornare malesuada ut, tincidunt eget dui. Pellentesque lacinia faucibus nibh, quis ornare nunc venenatis id. Maecenas eget nulla vel lorem hendrerit scelerisque sed in nisi. Curabitur pulvinar felis non purus iaculis viverra. Nulla euismod turpis non lacinia tincidunt. Nam eget scelerisque quam. Fusce a sodales lacus. Nam iaculis eget sapien vitae ornare. Donec ullamcorper eros eleifend eleifend ultrices.';
-		const lorem2 = 'Quisque fringilla sapien odio, et pretium arcu fermentum sit amet. Phasellus purus arcu, porta ultricies luctus quis, condimentum vitae ipsum. Ut id urna eget nisi varius cursus. Praesent varius finibus vehicula. Nunc vitae augue in ipsum cursus vulputate. Nulla feugiat nulla ut mauris vulputate, eget posuere lectus rhoncus. Praesent imperdiet mauris eu urna consequat scelerisque sed ut tortor. Aliquam suscipit purus ac lectus egestas, at imperdiet ipsum consectetur. Aenean quam ex, vehicula quis risus id, congue accumsan sapien. Sed ante augue, commodo ut faucibus id, consequat nec ex. Vestibulum eu velit malesuada, vulputate velit sed, fermentum lectus. Suspendisse tincidunt justo non leo viverra maximus. Aliquam feugiat lacus vitae est condimentum, vel dignissim dolor dictum.';
+		const bodyElements = this.props.body.map((bObj, index) => {
+			return (
+				<ParagraphAtom key={index.toString()}>
+					{bObj.content}
+				</ParagraphAtom>
+			);
+		});
+
 		return (
 			<div style={baseStyles.container}>
-				<PrimaryHeadingAtom>About</PrimaryHeadingAtom>
-				<ParagraphAtom>{lorem1}</ParagraphAtom>
-				<ParagraphAtom>{lorem2}</ParagraphAtom>
+				<PrimaryHeadingAtom>{this.props.title}</PrimaryHeadingAtom>
+				{bodyElements}
 			</div>
 		);
 	}
 };
 
 export default AboutEco;
+
+
+AboutEco.propTypes = {
+	body: React.PropTypes.arrayOf(React.PropTypes.shape({
+		imgSrc: React.PropTypes.string,
+		content: React.PropTypes.string
+	}))
+};
