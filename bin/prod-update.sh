a1ba97d2c894d5efa745e98c1f6d4791b02b796d
Jamie Syme <jamie.d.syme@gmail.com>
2017-03-04 09:04:00 -0800

    Added fix so that prod-update can be run right after prod-install.

--- bin/prod-update.sh
+++ bin/prod-update.sh
@@ -3,6 +3,10 @@
 # After using install-commands.sh
 # Be sure $PWD is /opt/portfolio
 
+# This is required if this is run immediately after prod-install.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 git pull origin master
 
 yarn install
