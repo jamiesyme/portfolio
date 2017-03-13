38f93f1dcdb8fb5ec8c9f484f258832c495f7611
Jamie Syme <jamie.d.syme@gmail.com>
2017-03-13 10:16:14 -0700

    Removed old lorem environment.

--- client/app.js
+++ client/app.js
@@ -2,7 +2,6 @@ import Radium from 'radium';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import LoremEnv from './environments/lorem';
 import NormalEnv from './environments/normal';
 
 
