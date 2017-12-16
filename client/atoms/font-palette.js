a416276c288e93ccb7d46df25c0192db4d0ee022
Jamie Syme <jamie.d.syme@gmail.com>
2017-12-15 21:22:19 -0800

    Deleted everything. We'll be starting the redesign from scratch.

--- client/atoms/font-palette.js
+++
@@ -1,7 +0,0 @@
-export const FontPaletteAtom = {
-	heavy: 'Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif',
-	//heavy: 'Tahoma, Geneva, sans-serif',
-	body: 'Georgia, Serif'
-};
-
-export default FontPaletteAtom;
