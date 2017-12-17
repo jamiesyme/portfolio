74dca50c3ebb7ba105bb8ab85f32a408e9f03c80
Jamie Syme <jamie.d.syme@gmail.com>
2017-12-17 15:34:12 -0800

    Automatically close the apps list by clicking on the desktop.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const $appsButton = $('.apps-button');
 const $appsList = $('.apps-list');
+const $desktop = $('.desktop');
 
 $appsButton.click(e => {
 	$appsList.toggle();
+});
+
+$desktop.click(e => {
+	if ($appsList.is(':visible')) {
+		$appsList.hide();
+	}
 });
